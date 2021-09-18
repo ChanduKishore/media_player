049663fe8b03b0cea6803fe2aab5481377a29698
ChanduKishore <kishorechandu223@gmail.com>
2021-09-18 07:41:59 +0530

    major update changes

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 session_start();
 if(isset($_SESSION['user'])){
 
-	header('Location:amzflip_video_tuts.php');
+	header('Location:media_player.php');
 }
 
 
@@ -13,45 +13,34 @@ if(isset($_SESSION['user'])){
 $title = 'Login';
 include 'header.php';
 
-
-
-
-$user_data=['user_name'=>'', 'password'=>''];
-
-// form validation
+$user_data=['username'=>''];
 
 if(isset($_POST['submit'])){
-
-	 		$_SESSION['user']=$user_data['user_name'];
-	 		$_SESSION['login']=true;
-	 		header('Location:amzflip_video_tuts.php');
-	 		 	
- 	
+	$user_data['username']=htmlspecialchars($_POST['username']);
+		if($_POST['username']!==''){
+	 			$_SESSION['user']=$user_data['username']; 				 	
+	 		 	}
+	 		 	else{
+	 		 		$_SESSION['user']='Stranger';		
+	 		 	}
+	 	$_SESSION['login']=true;
+	 	header('Location:media_player.php'); 
  }
  ?>
 
  
 <form action="login.php" method="post" novalidate="">
 	<h2 class="center capitalize mb-2">amzflip</h2>
-	<div><span class="form-error danger-text block center error-bg"><?php echo $errors['login_error'] ?></span></div>
 	<div>
-	<label for="user_name">Enter your Username</label>
-	<input class="<?php if($errors['user_name'] || $errors['login_error']) echo 'invalid' ?>" type="text" name="user_name" id="user_name" value="<?php echo $user_data['user_name'] ?>">
-	<span class="form-error danger-text"><?php echo $errors['user_name'] ?></span>
-</div>
-
-
+	<label for="username">Enter your Username</label>
+	<input class="<?php if($errors['username'] || $errors['login_error']) echo 'invalid' ?>" type="text" name="username" id="username" value="Stranger">
+	</div>
 
 <div>
 <button class='btn-width-full success' type="submit" name="submit" value="submit
 ">Submit</button>
 </div>
-`
 </form>
-
-<div class="card">
-	<p class="center">Don't have an account? <a class="blue link" href="sign_up.php">Sign Up</a> </p>
-</div>
 
 </body>
 </html>
--- media_player.php
+++ media_player.php
@@ -21,17 +21,17 @@ header('location:login.php');
 <head>
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-   	<title>amzflip tuts </title>
+   	<title>media player page </title>
     <link rel="stylesheet" type="text/css" href="sheet.css">
     <script type="text/javascript" src="script.js" defer></script>
 </head>
 <body>
 
 <header class="flex space_between pr-2  ">
-  <h2 class="brand capitalize white   ml-3">amzflip</h2>
+  <h2 class="brand capitalize white   ml-3"> COMPANY NAME</h2>
 <div class=" flex">
 <p class="user_logo" user-name="<?php echo $_SESSION['user']?>" ><?php echo $_SESSION['user'][0] ?></p>
-<form class="logout" method="post" action="amzflip_video_tuts.php"><input class=" fs-1-2 btn white" type="submit" value="Exit" name="logout" ></form>
+<form class="logout" method="post" action="media_player.php"><input class=" fs-1-2 btn white" type="submit" value="Exit" name="logout" ></form>
 </div>
 	
 </header>
@@ -39,7 +39,7 @@ header('location:login.php');
        
       <section >
         <div>
-        <iframe allowtransparency="true" title="amzflip_video_tuts" allowfullscreen frameborder="0" scrolling="no" src="" width="400" height="225"> video iframe </iframe>
+        <iframe allowtransparency="true" title="media_player" allowfullscreen frameborder="0" scrolling="no" src="" width="400" height="225"> video iframe </iframe>
         <p class="video-title">video title</p>
       </div>
       </section>
@@ -49,14 +49,14 @@ header('location:login.php');
         <select>
           <option value="module1">Marvel New Movie Trailers</option>
           <option value="module2">Bollywood New Release</option>
-          <option value="module3">Telugu Latest Release</option>        
+          <option value="module3">Telugu Latest Release</option>       
         </select>
       </div>
         <div class="aside_content"></div>
       </aside>
     </main>
-
 </body>
+
 
   </script>
 </html>
